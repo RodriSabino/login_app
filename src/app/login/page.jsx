8ebfaf4a01b94cc00098cade76b4f25af26e8110
Rodrigo Alexander Sabino Ramirez <u20201b801@upc.edu.pe>
2024-07-14 00:43:19 -0500

    Added Log in section

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,4 +1,5 @@
 import './global.css';
+import { FaFacebookF, FaGoogle } from 'react-icons/fa';
 
 function LoginPage() {
   return (
@@ -11,7 +12,24 @@ function LoginPage() {
       <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
         <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
           <div className="w-3/5 p-5">
-          <p>Sign in Section</p>
+            <div className="text-left font-bold">
+              <span className="text-green-500">React</span> Initializr
+            </div>
+            <div className="py-10">
+              <h2 className="text-3xl font-bold text-green-500 mb-2">Sign in to Account</h2>
+              <div className="border-2 w-10 border-green-500 inline-block mb-2"></div>
+              <div className="flex justify-center my-2">
+                <a href='#' className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                  <FaFacebookF className="text-sm" />
+                </a>
+                <a href='#' className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                  <FaGoogle className="text-sm" />
+                </a>
+              </div>
+              <p className="text-gray-500 text-sm">or use your email account:</p>
+              <div className="bg-gray-100"></div>
+            </div>
+          
           </div>
           <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
           <h2 className="text-3xl font-bold">Welcome Back</h2>
