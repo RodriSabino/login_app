f65c1eaaf4d47aeb9484831b69d2114c5392f226
Rodrigo Alexander Sabino Ramirez <u20201b801@upc.edu.pe>
2024-07-13 23:58:08 -0500

    added sign up section

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,16 +2,23 @@ import './global.css';
 
 function LoginPage() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100'>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-black'>
       <section id="up"></section>
       <section id="down"></section>
       <section id="left"></section>
       <section id="right"></section>
       
       <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-        <div className="bg-white rounded-2xl">
-          <div><p>Sign in Section</p></div>
-          <div><p>Sign up Section</p></div>
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+          <div className="w-3/5 p-5">
+          <p>Sign in Section</p>
+          </div>
+          <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+          <h2 className="text-3xl font-bold">Welcome Back</h2>
+          <div className="border-2 w-10 border-white inline-block mb-2"></div>
+          <p className="mb-10">Fill up personal information and start journey with us.</p>
+          <a href='#' className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500">Sign Up</a>
+          </div>
         </div>
       </main>
     </div>
